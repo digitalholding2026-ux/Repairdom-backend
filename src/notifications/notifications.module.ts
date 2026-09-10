7c16a467eb6885753d7aa6c21fc55d4c0a52bf23
digitalholding2026-ux <digitalholding2026@gmail.com>
2026-09-10 23:06:22 +0100

    fix: import AuthModule in NotificationsModule

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module.js';
 import { NotificationsController } from './notifications.controller.js';
 import { NotificationsService } from './notifications.service.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [NotificationsController],
   providers: [NotificationsService],
 })
